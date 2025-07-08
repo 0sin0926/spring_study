8d0ecd374f8aeeec9f5a54a5479368b9bce05e23
kim-youngsin <do730@naver.com>
2025-07-08 12:07:42 +0900

    mvc와 템플릿 엔진 개념

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,8 +1,9 @@
 package hello.hello_spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller // @Controller해서 annotation 적어줘야함
@@ -13,4 +14,12 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name); // parameter로 넘어온 "name"을 넘겨줌
+        return "hello-template";
+    }
+    // 여기서 name은 request했으니까 웹브라우저에서 주소에 ?name=spring!@ 이런식으로 따로 써서 넘겨줘야함
+    // ?~=~ 이런식으로 치면 파라미터에 String name과 model.addAttribute에 name이 입력한 그 값으로 바뀜
+    // template에 ${name}도 같이 이거로 바뀐다
 }
